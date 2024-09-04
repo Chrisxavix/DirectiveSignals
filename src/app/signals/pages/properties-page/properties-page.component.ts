b08d86881ebc23ef0fe9b92a12191d6fb2e8a3fe
Chrisxavix <chrischillogallid64@outlook.com>
2024-09-03 22:29:04 -0500

    signals - mutations

--- src/app/signals/pages/properties-page/properties-page.component.ts
+++ src/app/signals/pages/properties-page/properties-page.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, effect, signal } from '@angular/core';
+import { User } from '../../interfaces/user-request.interface';
 
 @Component({
   selector: 'app-properties-page',
@@ -6,5 +7,38 @@ import { Component } from '@angular/core';
   styleUrl: './properties-page.component.css'
 })
 export class PropertiesPageComponent {
+
+  public user = signal<User>({
+    "id": 2,
+    "email": "janet.weaver@reqres.in",
+    "first_name": "Janet",
+    "last_name": "Weaver",
+    "avatar": "https://reqres.in/img/faces/2-image.jpg"
+  });
+
+  public userChangeEffect = effect(() => {
+      console.log("UserChangeEffect");
+  });
+
+  onFieldUpdated(field: keyof User, value: string) {
+    /* console.log(field, value ); */
+
+    this.user.update( current => {
+      switch(field) {
+        case 'email':
+          current.email = value;
+          break;
+        case 'first_name':
+          current.first_name = value;
+          break;
+        case 'last_name':
+          current.last_name = value;
+          break;
+      }
+      return current;
+    })
+
+
+  }
 
 }
